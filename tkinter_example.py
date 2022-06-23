b0285ec2b9f488aad6dbd69b7048ca883c45c94d
alexsks65536 <megbyte@mail.ru>
2022-06-23 23:43:03 +0300

    tkinter

---
+++ tkinter_example.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import random
+
+
+window = tk.Tk()
+width_line = 100  # ширина полосы
+height_line = 1000  # высота полосы
+rainbow_line = ['white', 'yellow', 'aqua', 'green', 'magenta', 'red', 'blue', 'black']
+color_line = ['#FFFFFF', '#FFFF00', '#00FFFF', '#008000', '#800080', '#0000FF', '#000000']
+color_raw = list(range(0x000000, 0x0000FF))
+print(color_raw)
+# random.shuffle(rainbow_line)
+for i in color_line:
+    frame = tk.Frame(master=window, width=width_line, height=height_line, bg=i)
+    frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+window.mainloop()
--- Игра_21.py
+++ Игра_21.py
@@ -2,26 +2,35 @@ import tkinter as tk
 import random
 
 window = tk.Tk()
-greeting = tk.Label(
-    text="Привет, Tkinter!",
-    foreground="white",  # Устанавливает белый текст
-    background="#34A2FE",  # Устанавливает фон
-    width=20,  # ширина ярлыка
-    height=10  # высота ярлыка
-    )
-button = tk.Button(
-    text="Нажми на меня!",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="red",
-)
-entry = tk.Entry(fg="yellow", bg="blue", width=50)  # Виджет Entry — Однострочное текстовое поле
+# # параметры виджета
+# greeting = tk.Label(
+#     text="Привет, Tkinter!",
+#     foreground="white",  # Устанавливает белый текст
+#     background="#34A2FE",  # Устанавливает фон
+#     width=20,  # ширина ярлыка
+#     height=10  # высота ярлыка
+#     )
+# # параметры кнопки
+# button = tk.Button(
+#     text="Нажми на меня!",
+#     width=25,
+#     height=5,
+#     bg="blue",
+#     fg="red",
+# )
+# entry = tk.Entry(fg="yellow", bg="blue", width=50)  # Виджет Entry — Однострочное текстовое поле
+# entry.insert(0, "Иванов")  # можете вставить текст в виджете однострочного текстового поля с помощью метода
+# entry.insert(0, "Иван ")  # вставили следующее значение
 #  name = entry.get()
 #  entry.delete(0)
-greeting.pack()
-entry.pack()
-button.pack()
+# создаем текстовый виджет
+text_box = tk.Text()
+text_box.pack()
+#
+text_box.get("1.0")
+# greeting.pack()
+# entry.pack()
+# button.pack()
 window.mainloop()
 
 
